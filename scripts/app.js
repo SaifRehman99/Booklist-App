cb27a9d6f24f716eff3999754e449059cf7583a6
SaifRehman99 <saifr7493@gmail.com>
2019-05-18 13:16:46 +0500

    book constructor added

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+// Book Constructor here
+function Book(name, author, id, pages) {
+    this.name = name;
+    this.author = author;
+    this.id = id;
+    this.pages = pages;
+}
+
+// Creating the UI Constructor here
