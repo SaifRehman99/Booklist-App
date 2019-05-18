6878c2a3910e2458fbdc4766d7b94b83c17b7f3f
SaifRehman99 <saifr7493@gmail.com>
2019-05-18 13:22:49 +0500

    listener adding

--- scripts/app.js
+++ scripts/app.js
@@ -7,3 +7,23 @@ function Book(name, author, id, pages) {
 }
 
 // Creating the UI Constructor here
+function uiMaterial(){}
+
+// getting the reference here
+const form = document.querySelector('#bookApp');
+
+// adding the event listener
+form.addEventListener('submit',(e)=> {
+
+    // preventing the default behaviour
+    e.preventDefault();
+
+    // getting the form values
+    const name = document.querySelector('#bookName').value;
+    const author = document.querySelector('#bookAuthor').value;
+    const id = document.querySelector('#bookId').value;
+    const pages = document.querySelector('#bookPages').value;
+
+    // passing the values to the constructor function
+    const book = new Book(name,author,id,pages);
+})
